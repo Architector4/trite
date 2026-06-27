539aa28d6224d4124700d4457710069303cc5ac5
Architector #4 <architector4@mail.tilde.team>
2026-06-27 05:28:51 +0300

    bevy_mod_time_travel: improve basics example; remove the resource hack

--- bevy_mod_time_travel/examples/basics.rs
+++ bevy_mod_time_travel/examples/basics.rs
@@ -1,39 +1,44 @@
 use bevy::ecs::schedule::ScheduleLabel;
 use bevy::prelude::*;
-use bevy_ecs::component::{Mutable, StorageType};
+use bevy_ecs::component::Mutable;
 use bevy_mod_time_travel::prelude::*;
 
-// A timeline to time travel along
-#[derive(Clone, Debug, Deref, DerefMut, Reflect, Default)]
-struct MyTimeline<T: Clone + Send + Sync + 'static> {
+/// A timeline to time travel a singular component along.
+///
+/// It's a component in and of itself as well. An instance of a `MyTimeline<T>` component on an
+/// entity is responsible for tracking and rewinding the component `T` on the entity.
+#[derive(Component, Clone, Debug, Deref, DerefMut, Reflect, Default)]
+struct MyTimelineComp<T: Component<Mutability = Mutable> + Clone + Send + Sync + 'static> {
     buf: RewindBuffer<T>,
 }
 
-// If T is Component, then this is too.
-impl<T: Component<Mutability = Mutable> + Clone> Component for MyTimeline<T> {
-    const STORAGE_TYPE: StorageType = StorageType::Table;
-    type Mutability = Mutable;
+/// A timeline to time travel a singular resource along.
+///
+/// It's a resource in and of itself as well. An instance of a `MyTimeline<T>` resource in a world
+/// is responsible for tracking and rewinding the component `T` in that world.
+#[derive(Resource, Clone, Debug, Deref, DerefMut, Reflect, Default)]
+struct MyTimelineRes<T: Resource<Mutability = Mutable> + Clone + Send + Sync + 'static> {
+    buf: RewindBuffer<T>,
 }
 
-// If T is Resource, then this is too. Note: this is usually NOT the correct way to make a type a
-// Resource in Bevy. The `#[derive(Resource)]` macro also makes the type a component with the
-// `IsResource` type as a required component, without which the resource API is dysfunctional for
-// the type:
-// https://github.com/bevyengine/bevy/issues/24686
-//
-// This crate performs a workaround that makes this type act exactly like a proper resource when
-// needed, so feel free to define your resource timelines in this way. The workaround lives in the
-// function that registers the type as a resource timeline; see a bit below.
-impl<T: Resource<Mutability = Mutable> + Clone> Resource for MyTimeline<T> {}
-
-// Continuum type to group all instances of the generic timeline together.
+/// Continuum type to group all instances of the generic timeline components/resources together.
 #[derive(Clone, Copy, PartialEq, Eq, Debug, Hash, Default, Reflect, ScheduleLabel)]
 pub struct MyContinuum;
 
 // Implement the necessary traits.
+
 impl Continuum for MyContinuum {}
 
-impl<T: Clone + Send + Sync + 'static> Timeline for MyTimeline<T> {
+impl<T: Component<Mutability = Mutable> + Clone + Send + Sync + 'static> Timeline
+    for MyTimelineComp<T>
+{
+    type Item = T;
+    type Continuum = MyContinuum;
+}
+
+impl<T: Resource<Mutability = Mutable> + Clone + Send + Sync + 'static> Timeline
+    for MyTimelineRes<T>
+{
     type Item = T;
     type Continuum = MyContinuum;
 }
@@ -49,14 +54,12 @@ fn main() {
     // Register the timelines into the world. This creates the correct schedules and systems that will
     // perform the time travel.
     world
-        .register_timeline::<MyTimeline<Transform>>()
+        .register_timeline::<MyTimelineComp<Transform>>()
         .animatable()
         .reflect()
         .register_component();
-    // Same with one for the resource. This performs the aforementioned workaround that makes the
-    // type work like a resource properly.
     world
-        .register_timeline::<MyTimeline<SomeResource>>()
+        .register_timeline::<MyTimelineRes<SomeResource>>()
         // Standard linear interpolation formula
         .interpolate_with(|a, b, f| SomeResource(a.0 + (b.0 - a.0) * f))
         .register_resource();
@@ -64,12 +67,12 @@ fn main() {
     // Now we can perform all the good stuff.
 
     let ent = world
-        .spawn((Transform::default(), MyTimeline::<Transform>::default()))
+        .spawn((Transform::default(), MyTimelineComp::<Transform>::default()))
         .id();
 
     world.insert_resource(SomeResource(0.0));
     // For resources, the timeline is stored as a separate resource.
-    world.insert_resource(MyTimeline::<SomeResource>::default());
+    world.insert_resource(MyTimelineRes::<SomeResource>::default());
 
     assert!(world.get_resource::<SomeResource>().is_some());
 
@@ -109,7 +112,7 @@ fn main() {
     assert_eq!(new_resource.0, 0.5);
 }
 
-// Also run the above as a crate test lol
+// A quick wrapper to let `cargo test` run the above as a test lol
 #[test]
 fn main_but_as_a_test() {
     main();
