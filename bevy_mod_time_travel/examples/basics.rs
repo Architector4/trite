1572c2ffb53cd1b2dc00a110be5961f69f003b17
Architector #4 <architector4@mail.tilde.team>
2026-06-27 05:19:50 +0300

    bevy_mod_time_travel: move API example from readme to a separate file

---
+++ bevy_mod_time_travel/examples/basics.rs
@@ -0,0 +1,116 @@
+use bevy::ecs::schedule::ScheduleLabel;
+use bevy::prelude::*;
+use bevy_ecs::component::{Mutable, StorageType};
+use bevy_mod_time_travel::prelude::*;
+
+// A timeline to time travel along
+#[derive(Clone, Debug, Deref, DerefMut, Reflect, Default)]
+struct MyTimeline<T: Clone + Send + Sync + 'static> {
+    buf: RewindBuffer<T>,
+}
+
+// If T is Component, then this is too.
+impl<T: Component<Mutability = Mutable> + Clone> Component for MyTimeline<T> {
+    const STORAGE_TYPE: StorageType = StorageType::Table;
+    type Mutability = Mutable;
+}
+
+// If T is Resource, then this is too. Note: this is usually NOT the correct way to make a type a
+// Resource in Bevy. The `#[derive(Resource)]` macro also makes the type a component with the
+// `IsResource` type as a required component, without which the resource API is dysfunctional for
+// the type:
+// https://github.com/bevyengine/bevy/issues/24686
+//
+// This crate performs a workaround that makes this type act exactly like a proper resource when
+// needed, so feel free to define your resource timelines in this way. The workaround lives in the
+// function that registers the type as a resource timeline; see a bit below.
+impl<T: Resource<Mutability = Mutable> + Clone> Resource for MyTimeline<T> {}
+
+// Continuum type to group all instances of the generic timeline together.
+#[derive(Clone, Copy, PartialEq, Eq, Debug, Hash, Default, Reflect, ScheduleLabel)]
+pub struct MyContinuum;
+
+// Implement the necessary traits.
+impl Continuum for MyContinuum {}
+
+impl<T: Clone + Send + Sync + 'static> Timeline for MyTimeline<T> {
+    type Item = T;
+    type Continuum = MyContinuum;
+}
+
+// Example resource. The API for them is slightly different in this crate compared to components.
+#[derive(Resource, Clone, Default)]
+struct SomeResource(f32);
+
+fn main() {
+    // Let's go on a bizzare adventure.
+    let mut world = World::new();
+
+    // Register the timelines into the world. This creates the correct schedules and systems that will
+    // perform the time travel.
+    world
+        .register_timeline::<MyTimeline<Transform>>()
+        .animatable()
+        .reflect()
+        .register_component();
+    // Same with one for the resource. This performs the aforementioned workaround that makes the
+    // type work like a resource properly.
+    world
+        .register_timeline::<MyTimeline<SomeResource>>()
+        // Standard linear interpolation formula
+        .interpolate_with(|a, b, f| SomeResource(a.0 + (b.0 - a.0) * f))
+        .register_resource();
+
+    // Now we can perform all the good stuff.
+
+    let ent = world
+        .spawn((Transform::default(), MyTimeline::<Transform>::default()))
+        .id();
+
+    world.insert_resource(SomeResource(0.0));
+    // For resources, the timeline is stored as a separate resource.
+    world.insert_resource(MyTimeline::<SomeResource>::default());
+
+    assert!(world.get_resource::<SomeResource>().is_some());
+
+    // Store the current state into the timelines. This stores the state of ALL tracked components
+    // and resources with a corresponding timeline, which including the two inserted above.
+    world
+        .continuum::<MyContinuum>()
+        .insert_into_buffers(Duration::ZERO);
+
+    // Now edit the current world state.
+    world
+        .entity_mut(ent)
+        .get_components_mut::<&mut Transform>()
+        .unwrap()
+        .translation
+        .z = 1.0;
+
+    world.resource_mut::<SomeResource>().0 = 1.0;
+    assert!(world.get_resource::<SomeResource>().is_some());
+
+    // When you're about to do multiple things on a continuum, for brevity,
+    // it can be a good idea to grab the interface into a separate variable first.
+    let mut cont = world.continuum::<MyContinuum>();
+
+    // Store the new state too.
+    cont.insert_into_buffers(Duration::from_secs(1));
+
+    // Interpolate exactly halfway between the two states.
+    cont.interpolate_to(Duration::from_millis(500)).unwrap();
+
+    let new_transform = world.entity(ent).get_components::<&Transform>().unwrap();
+    // Waow! It interpolate!
+    assert_eq!(new_transform.translation.z, 0.5);
+
+    // The resource is, too!
+    let new_resource = world.resource::<SomeResource>();
+    assert_eq!(new_resource.0, 0.5);
+}
+
+// Also run the above as a crate test lol
+#[test]
+fn main_but_as_a_test() {
+    main();
+}
